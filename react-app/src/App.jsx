5fb95d48bb6ef5260b41a4ea0432b38989b3afa0
Hamza khan <hamzakhan.std25@gmail.com>
2025-08-20 20:01:17 +0500

    start chat bot

---
+++ react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+    <h1>
+      App Component
+      <span role="img" aria-label="rocket">🚀</span>
+      <span role="img" aria-label="star">⭐</span>
+      <span role="img" aria-label="heart">❤️</span>
+      <span role="img" aria-label="fire">🔥</span>
+    </h1>
+    </>
+  )
+}
+
+export default App
